399299efc71b2935314a29870885771c0bbccc47
admin_dho <davidhorph@hotmail.com>
2024-12-28 14:49:23 -0600

    There should be a MyList.add('The quick brown fox jumped over the lazy dog') that would throw an AttributeError. I did this mainly for testing. Note: If you want to test other error handling, you can comment out all the MyList.add(dict(...)) for other errors.

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 if __name__ == "__main__":
    from functions import LinkedList;
+   from colorama import init;
+   from termcolor import colored
 
+   init()
    MyList = LinkedList()
+   #Adding data to linked list.
    MyList.add(dict(_id='Jamie123', name='Jamie'));
    MyList.add(dict(_id='Kylie311', name='Kylie'));
    MyList.add(dict(_id='Acey591', name='Acey'));
    MyList.add(dict(_id='Jay319', name='Jay'));
    MyList.add(dict(_id='_KC191', name='K.C.'));
 
-   print(MyList, end="\n=========\n")
+   MyList.add("The quick brown fox jumped over the lazy dog...") #Should throw an exception error.
 
+   # Search person profile based on _id.
    print(MyList.id_search('Kylie311'));
+
+   print("\nLength of My List:",len(MyList), end="\n\n") #List length.
+   str1 = "="*37 + " CHAIN " + "="*37;
+   str2= "="*81;
+   print(colored(str1, color='red', attrs=['bold']))
+   print(MyList.ViewChain())
+   print(colored(str2, color='red', attrs=['bold']))
 
